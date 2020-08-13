5c94e12cdbf10ec02da92490c6a6b12b6109be29
Matthew James Wong <matthew.wong76@gmail.com>
2020-08-12 19:25:17 -0600

    Adding space and making the navbar do something

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,17 @@
 import React from 'react'
-import Nav from 'react-bootstrap/Nav'
-import Navbar from 'react-bootstrap/Navbar'
+import { Nav, Navbar} from "react-bootstrap";
 
 export default function NavBar() {
     return (
         <Navbar bg="light" variant="light" expand="lg">
             <Navbar.Brand>Matthew Wong</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#experience">Experience</Nav.Link>
-                    <Nav.Link href="#projects">Projects</Nav.Link>
-                    <Nav.Link href="#referrals">Referrals</Nav.Link>
+                    <Nav.Link href="/home">Home</Nav.Link>
+                    <Nav.Link href="/experience">Experience</Nav.Link>
+                    <Nav.Link href="/projects">Projects</Nav.Link>
+                    <Nav.Link href="/referrals">Referrals</Nav.Link>
                 </Nav> 
             </Navbar.Collapse>
         </Navbar>
