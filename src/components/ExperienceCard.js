0b5a9d35926434de2395558387248ddd0c2024c1
Matthew James Wong <matthew.wong76@gmail.com>
2020-08-15 19:36:39 -0600

    Adding experience cards

---
+++ src/components/ExperienceCard.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Card, Button} from 'react-bootstrap';
+import './ExperienceCard.css'
+
+export default function ExperienceCard({experience}) {
+
+    return (
+        <div className="row">
+            <div className="col-12 mt-3 mb-3">
+                <Card className="shadow">
+                    <Card.Body>
+                        <Card.Title>{ experience.company }</Card.Title>
+                        <Card.Subtitle className="mb-2">{ experience.title }</Card.Subtitle>
+                        <Card.Subtitle className="mb-2 text-muted">{ experience.location }</Card.Subtitle>
+                        <Card.Subtitle className="mb-2 text-muted">{ experience.time }</Card.Subtitle>
+                        <Card.Text>
+                            { experience.summary }
+                        </Card.Text>
+                        <Button variant="outline-primary">Show More</Button>
+                    </Card.Body>
+                </Card>
+            </div>
+        </div>
+    )
+}
--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import ExperienceCard from '../components/ExperienceCard.js';
+import Experiences from '../data/experience.json';
 
 export default function Experience() {
+
     return (
-        <div>
-            <p>Experience</p>
+        <div id = "experience">
+            <div className="container">
+                {
+                    Experiences.map((experience, index) => {
+                        return <ExperienceCard key={ index } experience={ experience }/>
+                    })
+                }
+            </div>
         </div>
     )
 }
