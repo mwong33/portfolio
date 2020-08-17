c71424225c00b4e4c5096e293a8312cb5b85f7a4
Matthew James Wong <matthew.wong76@gmail.com>
2020-08-17 16:40:11 -0600

    getting started with projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -6,7 +6,14 @@ import pageTransition from '../data/PageTransition.js';
 export default function Projects() {
     return (
         <motion.div id="projects" initial="out" animate="in" exit="out" variants={pageTransition}>
-            <p>Projects</p>
+            <div className="continer mt-5 mb-5">
+                <div className="row text-center">
+                    <div className="col-12">
+                        <h1>My Projects</h1>
+                        <p className="font-italic">More to come!</p>
+                    </div>
+                </div>
+            </div>
         </motion.div>
     )
 }
