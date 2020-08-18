0cfa450ac62f3c3cacca034227c7c6196ff33933
Matthew James Wong <matthew.wong76@gmail.com>
2020-08-17 19:34:05 -0600

    referral title and fixed project image height

--- src/pages/Referrals.js
+++ src/pages/Referrals.js
@@ -6,7 +6,14 @@ import pageTransition from '../data/PageTransition.js';
 export default function Referrals() {
     return (
         <motion.div id="referrals" initial="out" animate="in" exit="out" variants={pageTransition}>
-            <p>Referrals</p>
+            <div className="container">
+                <div className="row mt-5 mb-5 text-center">
+                    <div className="col-12">
+                        <h1>My Referrals</h1>
+                        <p className="font-italic">I had the privaledge to work and learn from some awesome people!</p>
+                    </div>
+                </div>
+            </div>
         </motion.div>
     )
 }
