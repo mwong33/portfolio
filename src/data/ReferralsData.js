203a3947d1f68b29b75583d19b10397f53f48a92
Matthew James Wong <matthew.wong76@gmail.com>
2020-08-24 12:27:48 -0600

    Adding dreamtek referral

--- src/data/ReferralsData.js
+++ src/data/ReferralsData.js
@@ -1,4 +1,5 @@
 import KarlHitePDF from '../pdf/MWong_KHite_Referral.pdf';
+import DreamTekPDF from '../pdf/MWong_DreamTek_Referral.pdf';
 
 export default [
     {
@@ -8,5 +9,13 @@ export default [
         "linkedin": "https://www.linkedin.com/in/karlhite/",
         "snippet": "Matthew is scrappy as hell. He’s got a great balance between figuring out solutions to his own problems and seeking help when he gets stuck. His work output is high and he anxiously seeks new challenges all the time.",
         "pdf": KarlHitePDF
+    }, 
+    {
+        "author": "Jozsef Foldi",
+        "position": "Principal",
+        "company": "DreamTek Industries",
+        "linkedin": "https://www.linkedin.com/in/jfoldi/",
+        "snippet": "Mr. Wong is the “hard work” exemplar. He was hand-selected as the Backend Team Lead for the Project Minions....He ensured quality of work and deftly managed to time and resource constraints. He took responsibility for tasks and commanded authority to achieve them. He frequently sought opportunities to better himself and his team - a sign of an excellent leader.",
+        "pdf": DreamTekPDF
     }
 ]
